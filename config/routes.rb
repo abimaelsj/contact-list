d749b2e4f62c53abd59e22b6ac5c83c71b4895f6
Abimael <abimaelsj@gmail.com>
2024-02-07 11:33:56 -0300

    Projeto finalizado

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,19 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  root to: "static_pages#index"
+  get 'sobre', to: 'static_pages#sobre'
+  get 'contato', to: 'static_pages#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
+
+  resources :contacts
+  resources :users, only: [:new, :create, :show, :edit, :update]
+
+
 end
